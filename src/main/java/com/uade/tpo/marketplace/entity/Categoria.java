8672663bd0f6a9740951063d7253fa08345a4cf9
Cecilia Taboada <ceciliataboada1@gmail.com>
2025-04-10 17:56:49 -0300

    se corrigen entidades

--- src/main/java/com/uade/tpo/marketplace/entity/Categoria.java
+++ src/main/java/com/uade/tpo/marketplace/entity/Categoria.java
@@ -1,5 +1,7 @@
 package com.uade.tpo.marketplace.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,7 +16,11 @@ public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String nombre;
+
+    @Column
     private String descripcion;
 
     public Categoria(String nombre, String descripcion) {
@@ -25,21 +31,18 @@ public class Categoria {
     public Categoria() {
     }
 
-    @Column
     public Long getId() {
         return id;
     }
 
-    @Column
     public String getNombre() {
         return nombre;
     }
 
-    @Column
     public String getDescripcion() {
         return descripcion;
     }
 
     @OneToMany(mappedBy = "categoria")
-    private Evento evento;
+    private List<Evento> evento;
 }
