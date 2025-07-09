f2eb4f59cc5abad5805dbc6639adca706332b013
lmontano <laura_mp1998@outlook.com>
2025-07-09 15:16:05 -0300

    update: manejo de imagenes

--- src/main/java/com/uade/tpo/marketplace/entity/Evento.java
+++ src/main/java/com/uade/tpo/marketplace/entity/Evento.java
@@ -1,8 +1,5 @@
 package com.uade.tpo.marketplace.entity;
 
-import java.sql.Date;
-import java.util.List;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,8 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+import java.util.Date;
+
 import com.uade.tpo.marketplace.enums.EstadoEvento;
 import lombok.Data;
 
@@ -22,7 +24,7 @@ public class Evento {
 
     public Evento() {
     }
-    public Evento(String nombre, String descripcion, Date fecha_hora, Artista artista, Locacion locacion, EstadoEvento estado, Categoria categoria, int cant_entradas, int pdescuento, String imagenEvento, String imagenZonas) {
+    public Evento(String nombre, String descripcion, Date fecha_hora, Artista artista, Locacion locacion, EstadoEvento estado, Categoria categoria, int cant_entradas, int pdescuento, byte[] imagenEvento, byte[] imagenZonas) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.fechaHora = fecha_hora;
@@ -47,6 +49,7 @@ public class Evento {
     private String descripcion;
     
     @Column
+    @Temporal(TemporalType.TIMESTAMP)
     private Date fechaHora;
     
     @ManyToOne
@@ -70,9 +73,11 @@ public class Evento {
     @Column
     private int pdescuento;
 
-    @Column
-    private String imagenEvento;
+    @Lob
+    @Column(name = "imagen_evento", columnDefinition="LONGBLOB")
+    private byte[] imagenEvento;
 
-    @Column
-    private String imagenZonas;
+    @Lob
+    @Column(name = "imagen_zonas", columnDefinition="LONGBLOB")
+    private byte[] imagenZonas;
 }
